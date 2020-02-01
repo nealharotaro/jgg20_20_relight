6f4b40e11abb75354fa89d48e1d4c1a01f44c7b9
KYLELLE WESLEY CHUA <kylellewesley.chua@benilde.edu.ph>
2020-02-02 00:31:34 +0800

    Automatically exits meter system mini game from winning or losing

--- Assets/Script/MeterSystem.cs
+++ Assets/Script/MeterSystem.cs
@@ -3,100 +3,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class MeterSystem : MonoBehaviour
+namespace UnityStandardAssets.Characters.FirstPerson
 {
-    [SerializeField] Generator myGenerator;
-    [SerializeField] MeshRenderer refuelLighting;
-    [SerializeField] Material activeLight;
-    [SerializeField] Material inActiveLight;
-    [SerializeField] Material blackLight;
-    [SerializeField] float speed = 5f;
-    [SerializeField] float refuelAmount = 100f;
-    public float currentPercent;
-    public float targetPercent;
-    public bool isLow;
-
-    [SerializeField] RectTransform arrow;
-
-    
-    private float elapsedTime = 0f;
 
 
-    // Start is called before the first frame update
-    void Start()
+    public class MeterSystem : MonoBehaviour
     {
-       
-    }
+        [SerializeField] Generator myGenerator;
+        [SerializeField] MeshRenderer refuelLighting;
+        [SerializeField] Material activeLight;
+        [SerializeField] Material inActiveLight;
+        [SerializeField] Material blackLight;
+        [SerializeField] float speed = 5f;
+        [SerializeField] float refuelAmount = 100f;
+        public float currentPercent;
+        public float targetPercent;
+        public bool isLow;
 
-    // Update is called once per frame
-    void Update()
-    {
-        arrowCirculation();
-        if (Input.GetKeyDown(KeyCode.Space))
+        [SerializeField] RectTransform arrow;
+
+
+        private float elapsedTime = 0f;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            StopArrow();
-            CalculatePercentage();
+
         }
 
-
-    }
-
-    private void CalculatePercentage()
-    {
-        if (targetPercent + .2f > 1)
+        // Update is called once per frame
+        void Update()
         {
-            if (currentPercent >= targetPercent || currentPercent <= targetPercent + .2f - 1)
+            arrowCirculation();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StopArrow();
+                CalculatePercentage();
+            }
+
+
+        }
+
+        private void CalculatePercentage()
+        {
+            var meter = arrow.GetComponentInParent<MeterSystem>();
+
+            if (targetPercent + .2f > 1)
+            {
+                if (currentPercent >= targetPercent || currentPercent <= targetPercent + .2f - 1)
+                {
+                    myGenerator.SetFuel(refuelAmount);
+                    isLow = false;
+                    refuelLighting.material = activeLight;
+                    meter.gameObject.SetActive(false);
+                    FindObjectOfType<FirstPersonController>().isPaused = false;
+
+
+
+                }
+            }
+            else if (currentPercent >= targetPercent && currentPercent <= targetPercent + .2f)
             {
                 myGenerator.SetFuel(refuelAmount);
                 isLow = false;
                 refuelLighting.material = activeLight;
+                meter.gameObject.SetActive(false);
+                FindObjectOfType<FirstPersonController>().isPaused = false;
+            }
+            else
+            {
+                StartCoroutine(ErrorMeter());
+                meter.gameObject.SetActive(false);
+                isLow = true;
 
-
+                FindObjectOfType<FirstPersonController>().isPaused = false;
             }
         }
-        else if (currentPercent >= targetPercent && currentPercent <= targetPercent + .2f)
+
+        private void arrowCirculation()
         {
-            myGenerator.SetFuel(refuelAmount);
-            isLow = false;
-            refuelLighting.material = activeLight;
-        }
-        else
-        {
-            isLow = true;
-            StartCoroutine(ErrorMeter());
-        }
-    }
-
-    private void arrowCirculation()
-    {
-        elapsedTime += speed * Time.deltaTime;
-        currentPercent = (elapsedTime % 101f) / 100f;
-        arrow.rotation = Quaternion.Euler(0, 0, 360 * currentPercent);
-    }
-
-
-    IEnumerator ErrorMeter()
-    {
-        while (isLow)
-        {
-            refuelLighting.material = inActiveLight;
-
-            yield return new WaitForSeconds(0.5f);
-
-            refuelLighting.material = blackLight;
+            elapsedTime += speed * Time.deltaTime;
+            currentPercent = (elapsedTime % 101f) / 100f;
+            arrow.rotation = Quaternion.Euler(0, 0, 360 * currentPercent);
         }
 
 
-    }
+        IEnumerator ErrorMeter()
+        {
+            while (isLow)
+            {
+                refuelLighting.material = inActiveLight;
 
-    public void StopArrow()
-    {
-        speed = 0f;
-    }
+                yield return new WaitForSeconds(0.5f);
 
-    public void StartArrow()
-    {
-        speed = 30f;
+                refuelLighting.material = blackLight;
+            }
+
+
+        }
+
+        public void StopArrow()
+        {
+            speed = 0f;
+        }
+
+        public void StartArrow()
+        {
+            speed = 30f;
+        }
     }
 }
